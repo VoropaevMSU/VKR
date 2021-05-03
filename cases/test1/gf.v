b019c9334cc169dd7a4099b5df441146ad0b32e1
Andrey <voropaev_andru@mail.ru>
2021-05-04 00:32:08 +0300

    Cases

--- cases/test1/gf.v
+++
@@ -1,8 +0,0 @@
-module top(in, x, y, out, out2);
-input in, x, y;
-output out, out2;
-
-or  a(out2, x, y);
-and b(out, in, x);
-
-endmodule
--- cases/test1/rf.v
+++
@@ -1,8 +0,0 @@
-module top(in, x, y, out, out2);
-input in, x, y;
-output out, out2;
-
-and a(out2, x, y);
-and c(out, in, x);
-
-endmodule
